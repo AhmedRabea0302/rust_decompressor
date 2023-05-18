cead4b6e5e01a8678c7f5f9d8994318091a25675
Ahmed Abo Elfadle <arabea169@gmail.com>
2023-05-18 13:07:54 +0200

    Added Files Permissions

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
 }
 
 fn real_main() -> i32 {
-    let args: Vec<_> = std::env::args::collect();
+    let args: Vec<_> = std::env::args().collect();
     if args.len() < 2 {
         println!("Usage:{} <Filename>", args[0]);
         return 1;
     } 
 
-    let fname = std::path::path::new(&*args[1]);
+    let fname = std::path::Path::new(&*args[1]);
     let file = fs::File::open(&fname).unwrap();
     let mut archive = zip::ZipArchive::new(file).unwrap(); 
     
@@ -36,9 +36,28 @@ fn real_main() -> i32 {
         } else {
             println!{
                 "File {} extracted to \"{}\" ({} bytes)",
+                file.name(),
                 outpath.display(),
-                file.size
+                file.size()
+            };
+
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+        // Adding Permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
             }
         }
     }
+    0
 }
