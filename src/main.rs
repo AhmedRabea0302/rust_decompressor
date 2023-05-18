cc510565e6416f1bbf729a08547d39857160b8bb
Ahmed Abo Elfadle <arabea169@gmail.com>
2023-05-18 12:45:50 +0200

    Added real_main fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::fs;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(real_main());
+}
+
+fn real_main() -> i32 {
+    let args: Vec<_> = std::env::args::collect();
+    if args.len() < 2 {
+        println!("Usage:{} <Filename>", args[0]);
+        return 1;
+    } 
+
+    let fname = std::path::path::new(&*args[1]);
+    let file = fs::File::open(&fname).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap(); 
+    
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("file {} comment:{}", i , comment);
+            }
+        }
+
+        if (*file.name()).ends_with("/") {
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!{
+                "File {} extracted to \"{}\" ({} bytes)",
+                outpath.display(),
+                file.size
+            }
+        }
+    }
 }
